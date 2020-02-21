433e3f778db17fe023a1716a2efcc03d0ffa9dd6
javaee <romain.capocasale99@gmail.com>
2020-02-21 08:55:26 +0100

    init project

---
+++ src/main/java/com/example/demo/SitnbeerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitnbeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitnbeerApplication.class, args);
+	}
+
+}
