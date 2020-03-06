323e713af6c78cd81921b03d3308461699f1b283
Romain Capocasale <romain.capocasale@he-arc.ch>
2020-03-06 11:23:51 +0100

    add bar validator

--- SitNBeer/src/main/java/com/example/demo/controllers/BarController.java
+++ SitNBeer/src/main/java/com/example/demo/controllers/BarController.java
@@ -1,14 +1,26 @@
 package com.example.demo.controllers;
 
+import java.security.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import com.example.demo.models.Bar;
+import com.example.demo.models.User;
+import com.example.demo.repositories.IUserRepository;
+import com.example.demo.validators.BarAddValidator;
 
 @Controller
 class BarController{
+
+	@Autowired
+	private BarAddValidator barAddValidator;
+
+	@Autowired
+	private IUserRepository userRepository;
 
     @GetMapping("/bar/add")
 	public String index(Model model) {
@@ -17,7 +29,18 @@ class BarController{
 	}
 
 	@PostMapping("/bar/add")
-	public String barSubmit(@ModelAttribute Bar bar, Model model) {
+	public String barSubmit(@ModelAttribute Bar bar, Model model, BindingResult bindingResult, Principal principal) {
+		
+		barAddValidator.validate(bar, bindingResult);
+
+		if(bindingResult.hasErrors()){
+			return "bar";
+		}
+
+		User loggedUser = userRepository.findByName(principal.getName());
+
+		bar.setUser(loggedUser);
+		
 		return "result";
 	}
 
--- SitNBeer/src/main/java/com/example/demo/validators/BarAddValidator.java
+++ SitNBeer/src/main/java/com/example/demo/validators/BarAddValidator.java
@@ -10,7 +10,22 @@ import com.example.demo.models.Bar;
 public class BarAddValidator implements Validator{
     @Override
     public void validate(Object o, Errors errors){
-        Bar 
+        Bar bar = (Bar)o;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "name.empty", "You must enter a bar name !");
+        if(bar.getName().length() < 2 || bar.getName().length() >32){
+            errors.rejectValue("name", "name.size", "The size must be between 2 and 32 !");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "address", "adress.empty", "You must enter a bar adress !");
+        if(bar.getAddress().length() < 10 || bar.getAddress().length() > 255){
+            errors.rejectValue("adress", "address.size", "The size must be between 10 and 255 !");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "availableTable", "availableTable.empty", "You must enter number of available table !");
+        if(bar.getAvailableTable() < 1 || bar.getAvailableTable() > 255){
+            errors.rejectValue("availableTable", "availableTable.size", "The size must be between 1 and 255 !");
+        }
     }
 
     public boolean supports(Class<?> aClass){
