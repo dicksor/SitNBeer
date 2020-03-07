3a9582496c3fe6d16f04d8510f9acb734750e409
JonasFreibur <jonas@TIC17-FREIBURGJ.intra.eiaj.ch>
2020-03-07 15:47:43 +0100

    add responsive navbar

--- SitNBeer/target/classes/static/js/app.js
+++ SitNBeer/target/classes/static/js/app.js
@@ -2,23 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
     var ranges  = document.querySelectorAll("input[type=range]");
     M.Range.init(ranges);
 
-    var elems = document.querySelectorAll('.dropdown-trigger')
+    var dropdowns = document.querySelectorAll('.dropdown-trigger')
     let dropdownOptions = {
         'hover': true,
         'constrainWidth': false
     }
-    var instances = M.Dropdown.init(elems, dropdownOptions)
+    var instances = M.Dropdown.init(dropdowns, dropdownOptions)
 
-
-    var sizeBarSlide = document.getElementById('pageSizeBars')
-    sizeBarSlide.onchange = function() {
-        const url = window.location.origin + window.location.pathname + "?size=" + this.value
-        window.location.assign(url)
-    }
-
-    var sizeBeerSlide = document.getElementById('pageSizeBeers')
-    sizeBeerSlide.onchange = function() {
-        const url = window.location.origin + window.location.pathname + "?size=" + this.value
-        window.location.assign(url)
-    }
+    var sideNavs = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(sideNavs);
 })
