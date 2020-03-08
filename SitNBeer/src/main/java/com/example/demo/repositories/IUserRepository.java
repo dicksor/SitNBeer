b897542ad354e10d740003f0b3b4c3bcdc8ffc44
Vincent Moulin <vincent.moulin@he-arc.ch>
2020-03-08 15:56:29 +0100

    registration verification oke, display msg now

--- SitNBeer/src/main/java/com/example/demo/repositories/IUserRepository.java
+++ SitNBeer/src/main/java/com/example/demo/repositories/IUserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IUserRepository extends JpaRepository<User, Long>{
     User findByUsername(String username);
+
+    User findByEmail(String email);
 }
--- SitNBeer/src/main/java/com/example/demo/validators/UserValidator.java
+++ SitNBeer/src/main/java/com/example/demo/validators/UserValidator.java
@@ -24,34 +24,39 @@ public class UserValidator implements Validator {
 		
 		User user = (User) o;
 
-        /*ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "username.empty", "You must enter a username!");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "username.empty", "You must enter a username!");
         if (user.getUsername().length() < 4 || user.getUsername().length() > 32) {
             errors.rejectValue("username", "username.size", "The size must be between 4 and 32!");
-        }*/
-        
-        /*if(!isValidEmail(user.getEmail())) {
-        	errors.rejectValue("email", "email.structure", "This is not a valid email!");
-        }*/
-        
-        /*if (userService.findByEmail(user.getEmail()) != null) {
-        	errors.rejectValue("email", "email.dupplicate", "This email is already used!");
-        }*/
+        }
 
-        /*ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "password.empty", "Password can't be empty!");
+        if (userService.findByUsername(user.getUsername()) != null) {
+        	errors.rejectValue("username", "username.dupplicate", "This username is already used!");
+        }
+        
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "email.empty", "You must enter an email!");
+        if(!isValidEmail(user.getEmail())) {
+        	errors.rejectValue("email", "email.structure", "This is not a valid email!");
+        }
+        
+        if (userService.findByEmail(user.getEmail()) != null) {
+        	errors.rejectValue("email", "email.dupplicate", "This email is already used!");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "password.empty", "Password can't be empty!");
         if (user.getPassword().length() < 8 || user.getPassword().length() > 32) {
         	errors.rejectValue("password", "password.length", "The size must be between 6 and 32!");
-        }*/
+        }
 
-        /*if (!user.getPasswordConfirm().equals(user.getPassword())) {
+        if (!user.getPasswordConfirm().equals(user.getPassword())) {
         	errors.rejectValue("passwordConfirm", "passwordConfirm.value", "This password doesn't match!");
-        }*/
+        }
         
 	}
 	
-	/*private boolean isValidEmail(String email)
+	private boolean isValidEmail(String email)
 	{
 		String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
 	    return email.matches(regex);
-	}*/
+	}
 
 }
