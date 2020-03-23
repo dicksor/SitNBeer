74d1c26782134378a6a87036fd99ac637a10dbe8
Romain Capocasale <romain.capocasale@he-arc.ch>
2020-03-23 23:20:21 +0100

    add katalon

--- SitNBeer/src/main/java/com/example/demo/controllers/UserController.java
+++ SitNBeer/src/main/java/com/example/demo/controllers/UserController.java
@@ -58,7 +58,7 @@ public class UserController {
         return "login";
     }
 
-    @GetMapping({ "/", "/home" })
+    @GetMapping("/home")
     public String welcome(Model model) {
         return "home";
     }
