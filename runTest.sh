21badf33d03b6393372ccda791fe7503f5036dd6
Jonas <jonas.freibs@gmail.com>
2020-04-15 15:15:38 +0200

    Update runTest.sh

--- runTest.sh
+++ runTest.sh
@@ -4,5 +4,5 @@ Xvfb :99 &
 export local_addr="127.0.0.1:8080"
 
 
-/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash  -runMode=console -projectPath="$(pwd)/SpringTest/SpringTest.prj" -retry=1 -testSuitePath="Test Suites/SpringTest" -executionProfile="default" -browserType="Firefox (headless)" -Djava.awt.headless
+/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash  -runMode=console -projectPath="$(pwd)/SitNBeerTest/SitNBeerTest.prj" -retry=1 -testSuitePath="Test Suites/TestSuite" -executionProfile="default" -browserType="Firefox (headless)" -Djava.awt.headless
 
