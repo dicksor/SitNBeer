ec23837142b04ef922701d40cd259f43b406ae8e
Romain <RomainCapo@users.noreply.github.com>
2020-04-15 17:45:48 +0200

    Update Script1586962198211.groovy

--- SitNBeerTest/Scripts/TestCaseBars/Script1586962198211.groovy
+++ SitNBeerTest/Scripts/TestCaseBars/Script1586962198211.groovy
@@ -15,7 +15,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.navigateToUrl('127.0.0.1:8181/bars')
+WebUI.navigateToUrl('http://127.0.0.1:8181/bars')
 
 WebUI.verifyTextPresent('Bars', false)
 
